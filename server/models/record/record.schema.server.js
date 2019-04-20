71961546e781f04ef635b5f4f2bb403a339d7766
Wenbo Wang <wang.wenbo@husky.neu.edu>
2019-04-20 16:29:44 -0700

    Minor update on record model
    Added owner field to the model.

--- server/models/record/record.schema.server.js
+++ server/models/record/record.schema.server.js
@@ -8,6 +8,7 @@ const recordSchema = new Schema({
   longitude: Number,
   latitude: Number,
   userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  owner: String,
   comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
   thumbUps: [String],
   date: String
