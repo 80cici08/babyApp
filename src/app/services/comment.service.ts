b3cdf1a3b6b96c072b5a9efd688f940c1eb43252
Wenbo Wang <wang.wenbo@husky.neu.edu>
2019-04-19 20:53:45 -0700

    Implemented record and comment client services
    Implemented record and comment client services

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  // the http calls URLs
+  private _createCommentUrl = '/api/record/';
+  private _findAllCommentsForRecordUrl = '/api/record/';
+  private _findAllCommentsForUserUrl = '/api/record/';
+  private _findCommentByIdUrl = '/api/comment/';
+  private _updateCommentUrl = '/api/comment/';
+  private _deleteCommentUrl = '/api/comment/';
+
+  constructor(private _http: HttpClient) {
+  }
+
+  // the http CRUD services
+
+  createComment(recordId: string, comment: any) {
+    return this._http.post<any>(
+      this._createCommentUrl + recordId + '/comment',
+      comment
+    );
+  }
+
+  findCommentsByRecordId(recordId: string) {
+    return this._http.get<any>(
+      this._findAllCommentsForRecordUrl + recordId + '/comment'
+    );
+  }
+
+  findCommentsByUserId(userId: string) {
+    return this._http.get<any>(
+      this._findAllCommentsForUserUrl + userId + '/comment'
+    );
+  }
+
+  findCommentById(commentId: string) {
+    return this._http.get<any>(this._findCommentByIdUrl + commentId);
+  }
+
+  updateComment(commentId: string, comment: any) {
+    return this._http.put<any>(this._updateCommentUrl + commentId, comment);
+  }
+
+  deleteComment(commentId: string) {
+    return this._http.delete<any>(this._deleteCommentUrl + commentId);
+  }
+
 }
