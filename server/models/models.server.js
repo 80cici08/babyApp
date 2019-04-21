59345151481f6712f350b550654354e0c62d891e
薛江阳 <xuejiangyang@xuejiangyangs-MacBook-Air.local>
2019-04-20 20:45:34 -0700

    update db info in models.server.js

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 // const db = "mongodb://localhost:27017/babyApp";
-console.log(process.env.MLAB_USERNAME_WEBDEV);
-const db = `mongodb://${process.env.MLAB_USERNAME_WEBDEV}:${process.env.MLAB_PASSWORD_WEBDEV}@ds145356.mlab.com:45356/heroku_hhjktfqg`;
+const db = `mongodb://admin:admin123@ds145356.mlab.com:45356/heroku_hhjktfqg`;
 mongoose.Promise = global.Promise;
 
 const dbConnection = mongoose.connect(
